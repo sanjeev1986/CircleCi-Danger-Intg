df4e7415a1f1588d5f640979f4f9ba56c369d46d
Sanjeev <sanjeev.sbr@gmail.com>
2020-06-22 14:04:08 +0200

    include danger

--- app/src/main/java/com/example/circlecidangetintegraion/MainActivity.kt
+++ app/src/main/java/com/example/circlecidangetintegraion/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.circlecidangetintegraion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Main Activity
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
